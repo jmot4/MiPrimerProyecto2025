eeb031a268c1680d369ead8a47d814cb658a73e1
jmot4 <jmota@gtech-services.com>
2025-08-26 15:34:43 -0600

    Se agrego nueva funcionalidad para despedirse

--- MiPrimerProyecto2025/Form1.cs
+++ MiPrimerProyecto2025/Form1.cs
@@ -21,5 +21,10 @@ namespace MiPrimerProyecto2025
         {
             MessageBox .Show ("Hola Mundo");
         }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Adiós Mundo");
+        }
     }
 }
